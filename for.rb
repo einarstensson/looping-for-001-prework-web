b813a97fd49066e35a6b339f17f878047353ba93
einarstensson <einar.stensson@gmail.com>
2016-01-27 14:50:41 -0500

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,9 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
+  for attempts in checklist
+    puts "Wingardium Leviosa"
+  end
 end
 
-
+#using_for  #Uncomment this line to run the program in the Terminal.
